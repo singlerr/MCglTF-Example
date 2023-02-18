e39a4c6c08351330f4eb06394bf48748311eba01
YTimLee <39574697+TimLee9024@users.noreply.github.com>
2023-02-18 21:55:33 +0800

    Make example items acquirable in creative tabs.

--- src/main/java/com/modularmods/mcgltf/example/Example.java
+++ src/main/java/com/modularmods/mcgltf/example/Example.java
@@ -1,6 +1,7 @@
 package com.modularmods.mcgltf.example;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.fabricmc.fabric.api.object.builder.v1.block.entity.FabricBlockEntityTypeBuilder;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
 import net.minecraft.core.Registry;
@@ -9,7 +10,9 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobCategory;
 import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.item.SpawnEggItem;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.entity.BlockEntityType;
@@ -57,5 +60,17 @@ public class Example implements ModInitializer {
 		
 		SpawnEggItem spawnEggItem = new SpawnEggItem(exampleEntityType, 12422002, 5651507,  new Item.Properties());
 		Registry.register(BuiltInRegistries.ITEM, new ResourceLocation("mcgltf", "example_entity_spawn_egg"), spawnEggItem);
+		
+		ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.FOOD_AND_DRINKS).register(listener -> {
+			listener.addAfter(Items.GLASS_BOTTLE, item);
+		});
+		
+		ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.FUNCTIONAL_BLOCKS).register(listener -> {
+			listener.addAfter(Items.JUKEBOX, blockItem);
+		});
+		
+		ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.SPAWN_EGGS).register(listener -> {
+			listener.addAfter(Items.VILLAGER_SPAWN_EGG, spawnEggItem);
+		});
 	}
 }
